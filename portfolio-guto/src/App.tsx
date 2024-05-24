9c80c16e927262e104147aa4236222cf7c77873f
Guto <gutodidonato@gmail.com>
2024-05-24 01:27:38 -0300

    projeto port

---
+++ portfolio-guto/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './ui/components/Navbar/Index.tsx';
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <p>Teste</p>
+    </>
+  )
+}
+
+export default App;
---
+++ portfolio-guto/src/ui/components/Navbar/Index.tsx
@@ -0,0 +1,38 @@
+import './style.css'
+
+
+
+
+function Navbar() {
+
+  function handleAtivacao(botao){
+    if (botao.target.className === 'Inativo'){
+      console.log(botao.target.className)
+      const botaoAnterior = document.querySelector('.Ativo');
+      botaoAnterior?.classList.replace('Ativo', 'Inativo');
+      botao.target.classList.replace('Inativo', 'Ativo');
+    }
+    
+  }
+  
+
+
+
+
+  return (
+      <header className='elemento-fodido'>
+        <nav className='header-nav'>
+            <ul className='header-listagem'>
+                <li><a onClick={(element) => handleAtivacao(element)} className='Ativo' >Home</a></li>
+                <li><a onClick={(element) => handleAtivacao(element)} className='Inativo'>Powers</a></li>
+            </ul>
+            <ul className='header-listagem'>
+                <li><a onClick={(element) => handleAtivacao(element)} className='Inativo'>Me</a></li>
+                <li><a onClick={(element) => handleAtivacao(element)} className='Inativo'>About</a></li>
+            </ul>
+        </nav>
+      </header>
+  )
+}
+
+export default Navbar;
